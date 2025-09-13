8e521d6ac10a5f0cf0d5604137c4f26b4b44fba4
dragonenderguy2 <shaihalapre@hotmail.com>
2025-09-13 10:00:30 -0500

    feat: initial commit for app.js

---
+++ app.js
@@ -0,0 +1,12 @@
+// Fichier principal JavaScript pour le Forum sur les Normes Ouvertes
+
+console.log('Bienvenue sur le Forum des Normes Ouvertes');
+
+// Code pour initialiser les fonctionnalités du forum ici
+
+function initForum() {
+    // Initialisation des forums et des discussions
+    console.log('Forum initialisé');
+}
+
+initForum();
